e059bf2015ea25cb46c1d18b784e8bcd15d7cfc5
StrawberryCreator <Sebbechewvanderkolk@icloud.com>
2025-02-10 15:28:23 +0100

    Add files via upload

---
+++ H27.py
@@ -0,0 +1,49 @@
+schoolInput = input("group 1 school name: ")
+nameInput = input("group 1 group name: ")
+sizeInput = input("group 1 size: ")
+placementInput = input("group 1 placement: ")
+
+group1 = (schoolInput, nameInput, sizeInput, placementInput)
+school, name, size, placement = group1
+
+print (group1)
+
+schoolInput = input("group 2 school name: ")
+nameInput = input("group 2 group name: ")
+sizeInput = input("group 2 size: ")
+placementInput = input("group 2 placement: ")
+
+group2 = (schoolInput, nameInput, sizeInput, placementInput)
+school, name, size, placement = group2
+
+print (group2)
+
+schoolInput = input("group 3 school name: ")
+nameInput = input("group 3 group name: ")
+sizeInput = input("group 3 size: ")
+placementInput = input("group 3 placement: ")
+
+group3 = (schoolInput, nameInput, sizeInput, placementInput)
+school, name, size, placement = group3
+
+print (group3)
+
+schoolInput = input("group 4 school name: ")
+nameInput = input("group 4 group name: ")
+sizeInput = input("group 4 size: ")
+placementInput = input("group 4 placement: ")
+
+group4 = (schoolInput, nameInput, sizeInput, placementInput)
+school, name, size, placement = group4
+
+print (group4)
+
+schoolInput = input("group 5 school name: ")
+nameInput = input("group 5 group name: ")
+sizeInput = input("group 5 size: ")
+placementInput = input("group 5 placement: ")
+
+group5 = (schoolInput, nameInput, sizeInput, placementInput)
+school, name, size, placement = group5
+
+print (group5)
