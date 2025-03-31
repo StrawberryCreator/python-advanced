05820a70d4b7fa2182a15544e73d39e933113665
StrawberryCreator <Sebbechewvanderkolk@icloud.com>
2025-03-31 16:29:28 +0200

    Add files via upload

--- L31/L31.py
+++ L31/L31.py
@@ -16,6 +16,10 @@ skipBox = Rect (690, 230, 100, 360)
 
 questionNum = 0
 questionCount = 0
+time = 15
+correct = 0
+
+isGameOver = False
 
 questions = []
 answerBoxes = [answerBox1, answerBox2, answerBox3, answerBox4]
@@ -33,6 +37,18 @@ def draw ():
     screen.draw.filled_rect (skipBox, "red")
     screen.draw.textbox (marqueeTxt, marqueeBox, color = "black")
     screen.draw.textbox (question[0].strip(), questionBox, color = "white")
+    screen.draw.textbox (str(time), timerBox)
+    screen.draw.textbox ("skip", skipBox, color = "dark red", angle = 270)
+    index = 1
+    for answerBox in answerBoxes:
+        screen.draw.textbox (question[index].strip(), answerBox, color = "black")
+        index += 1
+
+def gameOver ():
+    global question, time, isGameOver
+    question = [f"Game over! You got {correct} questions correctly", "", "", "", "", 5]
+    time = 15
+    isGameOver = True
 
 def marquee ():
     marqueeBox.x -= 2
@@ -52,10 +68,53 @@ def nextQst ():
     questionNum += 1
     return questions.pop (0).split (",")
 
+def timer ():
+    global time
+    global gameOver
+    time -= 1
+    if time == 0:
+        gameOver = True
+    clock.schedule (timer, 1)
+
+def on_mouse_down (pos):
+    global time, question
+    index = 1
+    for box in answerBoxes:
+        if box.collidepoint (pos):
+            print (int(question[5]))
+            print (index)
+            if index == int(question[5]):
+                question = nextQst ()
+                time = 15
+            else:
+                gameOver ()
+            index += 1
+    if skipBox.collidepoint (pos):
+        skip ()
+
+def CorrectAnswer ():
+    global correct, question, time, questions
+    correct += 1
+    if questions:
+        question = nextQst ()
+        time = 15
+    else:
+        gameOver ()
+
+def skip ():
+    global question, time, questions
+    if questions and not(isGameOver):
+        question = nextQst ()
+        time = 15
+    else:
+        gameOver ()
+
 def update ():
     marquee ()
 
 readFile ()
 question = nextQst ()
+
+clock.schedule (timer, 1)
 
 pg.go ()
